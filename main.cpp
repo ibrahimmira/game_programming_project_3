cd0087bb645799ab0e696dd4f27c6fb8316ae49e
ibrahimmira <135983387+ibrahimmira@users.noreply.github.com>
2025-10-15 12:05:23 -0400

    Setup main file

---
+++ main.cpp
@@ -0,0 +1,96 @@
+/**
+* Author: Ibrahim Mira
+* Assignment: Pong Clone
+* Date due: 2025-10-13, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+#include "CS3113/Entity.h"
+
+// Global Constants
+constexpr int SCREEN_WIDTH  = 1500,
+              SCREEN_HEIGHT = 800,
+              FPS           = 120;
+
+constexpr char BG_COLOUR[]    = "#000000ff";
+constexpr Vector2 ORIGIN      = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+
+constexpr float FIXED_TIMESTEP = 1.0f / 60.0f;
+
+// Global Variables
+AppStatus gAppStatus   = RUNNING;
+float gPreviousTicks   = 0.0f,
+      gTimeAccumulator = 0.0f;
+
+// Function Declarations
+void initialise();
+void processInput();
+void update();
+void render();
+void shutdown();
+
+void initialise()
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Lunar Lander");
+    SetTargetFPS(FPS);
+}
+
+void processInput() 
+{
+
+    if (IsKeyPressed(KEY_Q) || WindowShouldClose()) gAppStatus = TERMINATED;
+}
+
+void update() 
+{
+    // Delta time
+    float ticks = (float) GetTime();
+    float deltaTime = ticks - gPreviousTicks;
+    gPreviousTicks  = ticks;
+
+    // Fixed timestep
+    deltaTime += gTimeAccumulator;
+
+    if (deltaTime < FIXED_TIMESTEP)
+    {
+        gTimeAccumulator = deltaTime;
+        return;
+    }
+
+    while (deltaTime >= FIXED_TIMESTEP)
+    {
+        // Update game here and call use FIXED_TIMESTEP for any calls
+        deltaTime -= FIXED_TIMESTEP;
+    }
+}
+
+void render()
+{
+    BeginDrawing();
+    ClearBackground(ColorFromHex(BG_COLOUR));
+    EndDrawing();
+}
+
+void shutdown() 
+{ 
+    CloseWindow();
+}
+
+int main(void)
+{
+    initialise();
+
+    while (gAppStatus == RUNNING)
+    {
+        processInput();
+        update();
+        render();
+    }
+
+    shutdown();
+
+    return 0;
+}
